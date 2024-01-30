4473e4d8e08f5694994ac9ce4a9a55111cb0d83c
pranitrathod <pranitrathodsocial@gmail.com>
2024-01-30 16:59:34 +0530

    Updated player name without refs!

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
+import { useState } from 'react';
 import Player from './components/Player.jsx';
 
 function App() {
+  const [playerName,updatePlayerName]=useState("unKnown");
+  function setPlayerName(value)
+  {
+    if(value===""){
+      updatePlayerName("unKnown entity");
+    }else
+updatePlayerName(value);
+  }
   return (
     <>
-      <Player />
+      <Player unKnown={playerName} setName={setPlayerName}/>
       <div id="challenges"></div>
     </>
   );
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,22 @@
-export default function Player() {
+import { useState } from "react";
+
+export default function Player({unKnown,setName}) {
+  const [name,updateName]=useState("unKnown");
+  function setNameValue(event)
+  {
+    updateName(()=>event.target.value);
+  }
+  function save()
+  {
+    setName(name);
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {unKnown!=="unKnown"? unKnown: "unknown entity"}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" onChange={setNameValue}/>
+        <button onClick={save}>Set Name</button>
       </p>
     </section>
   );

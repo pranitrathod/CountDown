d2db2594aeba276da6af7863a484aae59455b09e
pranitrathod <pranitrathodsocial@gmail.com>
2024-02-03 16:03:31 +0530

    use-case of ref's

--- src/components/TimerChange.jsx
+++ src/components/TimerChange.jsx
@@ -1,13 +1,39 @@
+import { useState,useRef } from "react";
+
 export default function TimerChange({ title, targetTime }) {
-  return(
-  <section className="challenge">
-    <h2>{title}</h2>
-    <p className="challenge-time">
-    {targetTime} {targetTime>1 ? 'seconds' : 'second'}
-    </p>
-    <p>
-      <button>Start Challenge</button>
-    </p>
-    <p className="">Time is running!!!!</p>
-  </section>);
+  const time=useRef();
+  // const [checkRunning, setCheckRunning] = useState(false);
+  const [timerExpired, setTimerExpired] = useState(false);
+  const [timerStarted, setTimerStarted] = useState(false);
+
+  function handleStart() {
+    
+  time.current= setTimeout(() => {
+  
+      console.log("inside setTimeout!")
+      setTimerExpired(true);
+    },targetTime*1000);
+    setTimerStarted(true);
+    
+  }
+  function clearTime()
+  {
+    clearTimeout(time.current);
+  }
+  return (
+    <section className="challenge">
+      <h2>{title}</h2>
+      {timerExpired && <p>You lost!</p>}
+  
+      <p className="challenge-time">
+        {targetTime} {targetTime > 1 ? "seconds" : "second"}
+      </p>
+      <p>
+        <button onClick={timerStarted ? clearTime:handleStart}>
+          {timerStarted ? "Stop Challenge" : "Start Challenge"}
+        </button>
+      </p>
+      <p>{timerStarted ? "Time is running!!!!" : "Inactive"}</p>
+    </section>
+  );
 }
